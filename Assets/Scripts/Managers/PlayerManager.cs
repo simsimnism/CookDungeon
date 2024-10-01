a3efe15f13141df00b356c29b3f55cb97ff0bd3b
pgu1213 <pgu5136@gmail.com>
2024-10-02 02:19:23 +0900

    [ADD] 매니저, 리소스 개편
    매니저 개편
    리소스 폴더 생성 (앞으로 스프라이트, 프리팹 등 리소스는 이 폴더 내에 위치 할 예정)
    던전 빌더 오류 해결 중

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager
+{
+
+    // �̱��� �ν��Ͻ�
+    public static PlayerManager Instance { get; private set; }
+
+    // �÷��̾� �̵� �� �ִϸ��̼� ����
+    public float speed = 3.0f;
+    public Vector2 inputVec;
+    public string upAinme = "PlyerUp";
+    public string downAinme = "PlyerDown";
+    public string rightAinme = "PlyerRight";
+    public string LeftAinme = "PlyerLeft";
+    public string deadAinme = "PlayerDead";
+
+    public int hp = 3;
+    public string gameState = "playing";
+    public bool inDamage = false;
+
+    // �뽬 ���� ����
+    public float dashSpeedMultiplier = 2.0f; // �뽬 �� �ӵ� ���
+    public float dashDuration = 0.2f; // �뽬 ���� �ð�
+    public float dashCooldown = 1.0f; // �뽬 ��Ÿ��
+
+    public void Init()
+    {
+        // �ʱ�ȭ �۾�
+        hp = 3;
+        gameState = "playing";
+        inDamage = false;
+    }
+}
