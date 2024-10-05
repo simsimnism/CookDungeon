9d8ff58d170ea7479f65c6e68153f878cb64194d
pgu1213 <pgu5136@gmail.com>
2024-10-05 15:26:59 +0900

    [ADD] 수동 머지
    매니저, 스크립터블 오브젝트가 머지 안되서 수동으로 파일 넣음.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using TMPro;
+using UnityEngine;
+
+public class GameManager
+{
+    // 던전 레벨 리스트 설정
+    private List<DungeonLevelSO> dungeonLevelList;
+    // 초기 던전 레벨 값 (스테이지 번호)
+    private int currentDungeonLevelListIndex = 0;
+
+    public void Init()
+    {
+        dungeonLevelList = new List<DungeonLevelSO>();
+
+        // 리소스에서 던전 레벨 SO 로드
+        DungeonLevelSO[] DungeonLevel = Resources.LoadAll<DungeonLevelSO>("ScriptableObjectAssets/Dungeon/Level");
+
+        dungeonLevelList.AddRange(DungeonLevel);
+
+        if (currentDungeonLevelListIndex >= 0 && currentDungeonLevelListIndex < dungeonLevelList.Count)
+        {
+            genDungeon(currentDungeonLevelListIndex);
+        }
+        else
+        {
+            Debug.LogError("유효하지 않은 던전 레벨 인덱스: " + currentDungeonLevelListIndex);
+        }
+    }
+
+    void genDungeon(int dungeonLevelListIndex)
+    {
+        bool dungeonBuiltSucessfully = DungeonBuilder.Instance.GenerateDungeon(dungeonLevelList[dungeonLevelListIndex]);
+
+        if (!dungeonBuiltSucessfully)
+        {
+            Debug.LogError("던전 생성 실패 - 지정된 방과 노드 그래프에서 던전을 만들 수 없습니다.");
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/Monster Manager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+    public MonsterData[] normalMonsters;   // 일반 몬스터 배열
+    public MonsterData[] specialMonsters;  // 특수 몬스터 배열
+
+    private string jsonFilePath = "monsters";  // Resources 폴더 안에 monsters.json 파일
+
+    void Start()
+    {
+        LoadMonsterData();
+    }
+
+    // JSON 파일에서 몬스터 데이터를 불러오는 함수
+    void LoadMonsterData()
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>(jsonFilePath);
+
+        if (jsonFile != null)
+        {
+            Monsters monstersList = JsonUtility.FromJson<Monsters>(jsonFile.text);
+
+            // NormalMonsters와 SpecialMonsters 데이터를 저장
+            normalMonsters = monstersList.monster.NormalMonsters;
+            specialMonsters = monstersList.monster.SpecialMonsters;
+
+            // 데이터를 확인하는 로그 출력 (디버깅용)
+            Debug.Log("일반 몬스터 수: " + normalMonsters.Length);
+            Debug.Log("특수 몬스터 수: " + specialMonsters.Length);
+        }
+        else
+        {
+            Debug.LogError("몬스터 JSON 파일을 찾을 수 없습니다.");
+        }
+    }
+
+    // 특정 ID의 몬스터 데이터를 가져오는 함수
+    public MonsterData GetMonsterDataByID(int id)
+    {
+        foreach (MonsterData monster in normalMonsters)
+        {
+            if (monster.id == id)
+            {
+                return monster;
+            }
+        }
+
+        foreach (MonsterData monster in specialMonsters)
+        {
+            if (monster.id == id)
+            {
+                return monster;
+            }
+        }
+
+        return null;
+    }
+}
+
