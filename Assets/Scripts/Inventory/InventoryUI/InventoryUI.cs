e6ce166035274f32d3e4c3aad2a439f0bfafcf48
EungLee <147842273+EungLee@users.noreply.github.com>
2024-10-24 09:56:26 +0900

    10/24
    인벤토리 관련 스크립트 생성

---
+++ Assets/Scripts/Inventory/InventoryUI/InventoryUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//인벤토리 UI 관련 스크립트 헤드라인 이동, 팝업관리, 아이템 슬롯 등 종합적인 UI 관리가 이뤄지는 코드
+public class InventoryUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
