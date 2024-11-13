23b2c377eff6030da34f6f8e19d69adf34a78d07
pgu1213 <pgu5136@gmail.com>
2024-11-13 12:36:52 +0900

    [ADD] 빌드 세팅
    빌드 가능하게 수정

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 [RequireComponent(typeof(CinemachineTargetGroup))]
 
