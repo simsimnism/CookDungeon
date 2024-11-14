c38cc34b50e04689591ec3ac2fae1b39a8ba8bcd
이준규 <133856313+simsimnism@users.noreply.github.com>
2024-11-15 04:21:56 +0900

    [ADD] UI scale 값 조정

--- Assets/Scripts/Cooking/CookingPopup.cs
+++ Assets/Scripts/Cooking/CookingPopup.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 //요리UI의 팝업을 담당
 public class CookingPopup : UI_Popup
 {
-
     void Start()
     {
         base.Init();
     }
-
     public void CloseCook()
     {
+        
         Managers.Popup.CloseCooking();
     }
 }
