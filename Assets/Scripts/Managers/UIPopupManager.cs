fac4da79298564e1515664b4ffd37723e16fabf6
이준규 <133856313+simsimnism@users.noreply.github.com>
2024-11-15 23:11:27 +0900

    [ADD] 레시피 팝업 UI 제작 완

--- Assets/Scripts/Managers/UIPopupManager.cs
+++ Assets/Scripts/Managers/UIPopupManager.cs
@@ -396,7 +396,7 @@ public class UIPopupManager
     /*사용 설명서
     if (_RecipePopup == null)
         {
-            _RecipePopup = Managers.UI.ShowPopupUI<스크립트 이름>("열고자 하는 캔버스 오브젝트 이름");
+            _RecipePopup = Managers.UI.ShowPopupUI<RecipePopup>("열고자 하는 캔버스 오브젝트 이름");
         }
     */
 
@@ -404,7 +404,7 @@ public class UIPopupManager
     {
         if (_RecipePopup == null)
         {
-            _RecipePopup = Managers.UI.ShowPopupUI<RecipePopup>("Recipe");
+            _RecipePopup = Managers.UI.ShowPopupUI<RecipePopup>("RecipePopUI");
         }
         else
         {
