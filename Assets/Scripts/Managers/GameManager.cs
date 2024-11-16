ea8e1adb079dd59d10995ff59983598996e86569
EungLee <147842273+EungLee@users.noreply.github.com>
2024-11-17 01:27:36 +0900

    11/17
    수정완 거의

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -102,6 +102,8 @@ public class GameManager
         GameObject Player = Managers.Resource.Instantiate("Player/Player");
         player = Player.GetComponent<Player>();
 
+        Managers.Popup.OpenGameClear();
+
         // 카메라 세팅
         GameObject Camera = Managers.Resource.Instantiate("Camera/PlayerCamera");
 
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -42,9 +42,6 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        // 4초 후에 게임 클리어 UI 팝업을 띄우는 코루틴 시작
-        StartCoroutine(SpawnGameClearPopup());
-        StartCoroutine(GameClearPopup());
 
         currentHP = MaxHP;
 
@@ -53,22 +50,6 @@ public class Player : MonoBehaviour
         {
             hpBar.SetHealthBarValue((float)currentHP / (float)MaxHP);
         }
-    }
-
-    private IEnumerator SpawnGameClearPopup()
-    {
-        yield return new WaitForSeconds(2f);
-
-        // Managers.Popup을 통해 게임 클리어 팝업 호출
-        Managers.Popup.OpenGameClear();
-    }
-
-    private IEnumerator GameClearPopup()
-    {
-        yield return new WaitForSeconds(2.01f);
-
-        // Managers.Popup을 통해 게임 클리어 팝업 호출
-        Managers.Popup.CloseGameClear();
     }
 
     private IEnumerator Intro()
